de4515c569085d7d85fa2e9bb995b3963d1d9d8c
Juan Diego Prochazka <juandiegopro1@gmail.com>
2025-10-18 15:58:36 -0500

    backend- broad changes

--- backend/modal_server.py
+++ backend/modal_server.py
@@ -8,6 +8,7 @@ image = modal.Image.debian_slim().pip_install([
     "fastapi[standard]",
     "pillow",
     "python-multipart"
+    "torch"
 ])
 
 volume = modal.Volume.from_name(name="ut3c-heritage")
@@ -29,12 +30,12 @@ def fastapi_app():
     import io
     from contextlib import asynccontextmanager
     import shutil
+    import torch
 
     web_app = FastAPI()
     Pi3Remote = modal.Cls.from_name("pi3-inference", "ModelInference")
     inference_obj = Pi3Remote()
 
-    image_hashes = {}
     @asynccontextmanager
     def lifespan():
         folder_path = vol_mnt_loc / "backend_data" / "reconstructions" / "auditorio" / "images"
@@ -42,9 +43,16 @@ def fastapi_app():
         folder_path.mkdir(parents=True, exist_ok=True)
         print(f"Initialized folder: {folder_path}")
         yield
+    
 
-    @web_app.post("/pointcloud/{id}")
     async def upload_image(id: str, file: UploadFile = File(...)):
+        """
+            Takes a project id and a file and preprocesses and puts the image 
+            into the correct path
+            id: str an id that matches one of the existing reconstructions
+            file: UploadFile a file represnrting an image
+            returns: Path, the direction of the stored image
+        """
         path = vol_mnt_loc / "backend_data" / "reconstructions" / id / "images"
         if not os.path.exists(path) or not os.path.isdir(path):
             raise HTTPException(status_code=400,
@@ -71,20 +79,41 @@ def fastapi_app():
         else:
             new_height = target_size
             new_width = int(width * target_size / height)
-        
-
 
         img = img.resize((new_width, new_height), Image.BICUBIC)
         uuidname = uuid.uuid4()
         img.save(path / f"{uuidname}.png", format="PNG")
+        return path / f"{uuidname}.png"
+
+    async def run_inference(id: str):
+        """
+        Runs inference on the specified project.
+        id: the id of a project
+        returns: Path the path of the .pt object
+        """
+        path = vol_mnt_loc / "backend_data" / "reconstructions" / id / "images"
+        return inference_obj.run_inference.remote(vol_mnt_loc)
+    
+    def process_infered_data(inf_data_path,pty_location,conf_thresh = 50):
+        """
+        """
+        data = torch.load(inf_data_path)
+        for i in data:
+            print(i)
+
+
+    @web_app.post("/pointcloud/{id}")
+    async def new_image(id: str, file: UploadFile = File(...)):
+        uploaded = await upload_image(id,file)
+        inference_path = run_inference()
+        process_infered_data(inference_path,"")
         
         return JSONResponse(
-                content={"status": "OK", "message": f"File {uuidname}.png  created successfully"},
+                content={"status": "OK", "message": f"File {uploaded.name}.png  created successfully"},
                 status_code=status.HTTP_200_OK)
 
     @web_app.get("/pointcloud/{pc_id}/{tag}")
     async def get_pointcloud(pc_id: str, tag: str):
-        path = vol_mnt_loc / "backend_data" / "reconstructions" / pc_id / "images"
-        print(inference_obj.run_inference.remote(vol_mnt_loc))
+        pass
 
     return web_app
