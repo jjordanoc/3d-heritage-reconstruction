d47ab3430d2fe907e67b77e1328779fa1458f50a
jjordanoc <joaquin.jordan@utec.edu.pe>
2025-10-18 20:00:15 -0500

    next front

---
+++ ut3c-heritage/src/app/viewer/page.tsx
@@ -0,0 +1,11 @@
+import PLYViewer from '@/components/PLYViewer';
+
+const ViewerPage = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <PLYViewer />
+    </div>
+  );
+};
+
+export default ViewerPage;
